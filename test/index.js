19d4dd34b66b05ee35362a6b2ec9fb4d2d215e05
Caserage <schnieppbenedikt@icloud.com>
2020-04-04 14:28:29 +0200

    Project creation. Initial commit.

---
+++ test/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const chai = require("chai");
+
+const expect = chai.expect;
+
+describe("Index", () => {
+    it("Returns true", () => {
+        expect(true).to.deep.eq(true);
+    });
+});
