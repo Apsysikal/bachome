def3148142c945aac541e8badd3472c0da220efc
Caserage <schnieppbenedikt@gmail.com>
2020-04-23 12:57:25 +0200

    Set up some wrapper functions. Still needs testing.

--- bacnet.js
+++ bacnet.js
@@ -3,6 +3,7 @@
 /// <reference path="./node_modules/bacstack/lib/enum.js" />
 
 const bacnet = require("bacstack");
+const EventEmitter = require("events");
 
 const client = new bacnet();
 
@@ -24,3 +25,105 @@ client.readProperty("192.168.1.147", obj, bacnet.enum.PropertyIds.PROP_PRESENT_V
 
     console.log(value);
 });
+
+function readBinaryInput(ipAddress, instance, propertyId) {
+    return new Promise((resolve, reject) => {
+        const propertyObject = {
+            type: bacnet.enum.ObjectTypes.OBJECT_BINARY_INPUT,
+            instance: instance
+        };
+
+        client.readProperty(ipAddress, propertyObject, propertyId, (error, value) => {
+            if (error) {
+                return reject(error);
+            }
+
+            return resolve(value);
+        });
+    });
+}
+
+function readBinaryValue(ipAddress, instance, propertyId) {
+    return new Promise((resolve, reject) => {
+        const propertyObject = {
+            type: bacnet.enum.ObjectTypes.OBJECT_BINARY_VALUE,
+            instance: instance
+        };
+
+        client.readProperty(ipAddress, propertyObject, propertyId, (error, value) => {
+            if (error) {
+                return reject(error);
+            }
+
+            return resolve(value);
+        });
+    });
+}
+
+function readBinaryOutput(ipAddress, instance, propertyId) {
+    return new Promise((resolve, reject) => {
+        const propertyObject = {
+            type: bacnet.enum.ObjectTypes.OBJECT_BINARY_OUTPUT,
+            instance: instance
+        };
+
+        client.readProperty(ipAddress, propertyObject, propertyId, (error, value) => {
+            if (error) {
+                return reject(error);
+            }
+
+            return resolve(value);
+        });
+    });
+}
+
+function readAnalogInput(ipAddress, instance, propertyId) {
+    return new Promise((resolve, reject) => {
+        const propertyObject = {
+            type: bacnet.enum.ObjectTypes.OBJECT_ANALOG_INPUT,
+            instance: instance
+        };
+
+        client.readProperty(ipAddress, propertyObject, propertyId, (error, value) => {
+            if (error) {
+                return reject(error);
+            }
+
+            return resolve(value);
+        });
+    });
+}
+
+function readAnalogValue(ipAddress, instance, propertyId) {
+    return new Promise((resolve, reject) => {
+        const propertyObject = {
+            type: bacnet.enum.ObjectTypes.OBJECT_ANALOG_VALUE,
+            instance: instance
+        };
+
+        client.readProperty(ipAddress, propertyObject, propertyId, (error, value) => {
+            if (error) {
+                return reject(error);
+            }
+
+            return resolve(value);
+        });
+    });
+}
+
+function readAnalogOutput(ipAddress, instance, propertyId) {
+    return new Promise((resolve, reject) => {
+        const propertyObject = {
+            type: bacnet.enum.ObjectTypes.OBJECT_ANALOG_OUTPUT,
+            instance: instance
+        };
+
+        client.readProperty(ipAddress, propertyObject, propertyId, (error, value) => {
+            if (error) {
+                return reject(error);
+            }
+
+            return resolve(value);
+        });
+    });
+}
