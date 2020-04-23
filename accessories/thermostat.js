23390d9ce82efe932265773969f6a9af9e941b05
Caserage <schnieppbenedikt@icloud.com>
2020-04-23 17:38:23 +0200

    Updated bacnet helper.

--- accessories/thermostat.js
+++ accessories/thermostat.js
@@ -1,143 +1,189 @@
 "use strict";
 
+const bacnet = require("bacstack");
+const bacnetHelper = require("../bacnet");
+
 let Service, Characteristic, UUID;
 
 function myThermostat(log, config) {
-    this.log = log;
+  this.log = log;
 
-    this.manufacturer = "My manufacturer";
-    this.model = "My model";
-    this.name = config["name"] || "My Thermostat";
-    this.serial = "123-456-789";
+  this.manufacturer = "My manufacturer";
+  this.model = "My model";
+  this.name = config["name"] || "My Thermostat";
+  this.serial = "123-456-789";
 
-    this.currentHeatingCoolingState = 0; // 0 = OFF, 1 = HEAT, 2 = COOL
-    this.targetHeatingCoolingState = 0; // 0 = OFF, 1 = HEAT, 2 = COOL, 3 = AUTO
-    this.currentTemperature = 20.0;
-    this.targetTempearture = 25.0;
-    this.temperatureDisplayUnits = 0; // 0 = CELSIUS, 1 = FAHRENHEIT
-};
+  this.currentHeatingCoolingState = 0; // 0 = OFF, 1 = HEAT, 2 = COOL
+  this.targetHeatingCoolingState = 0; // 0 = OFF, 1 = HEAT, 2 = COOL, 3 = AUTO
+  this.currentTemperature = 20.0;
+  this.targetTempearture = 25.0;
+  this.temperatureDisplayUnits = 0; // 0 = CELSIUS, 1 = FAHRENHEIT
+}
 
 myThermostat.prototype.getServices = function () {
-    let infoService = new Service.AccessoryInformation()
-        .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
-        .setCharacteristic(Characteristic.Model, this.model)
-        .setCharacteristic(Characteristic.SerialNumber, this.serial);
+  let infoService = new Service.AccessoryInformation()
+    .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+    .setCharacteristic(Characteristic.Model, this.model)
+    .setCharacteristic(Characteristic.SerialNumber, this.serial);
 
-    let thermostatService = new Service.Thermostat(this.name);
+  let thermostatService = new Service.Thermostat(this.name);
 
-    thermostatService
-        .getCharacteristic(Characteristic.CurrentHeatingCoolingState)
-        .on("get", this.getCurrentHeatingCoolingState.bind(this))
-        .on("set", this.setCurrentHeatingCoolingState.bind(this));
+  thermostatService
+    .getCharacteristic(Characteristic.CurrentHeatingCoolingState)
+    .on("get", this.getCurrentHeatingCoolingState.bind(this))
+    .on("set", this.setCurrentHeatingCoolingState.bind(this));
 
-    thermostatService
-        .getCharacteristic(Characteristic.TargetHeatingCoolingState)
-        .on("get", this.getTargetHeatingCoolingState.bind(this))
-        .on("set", this.setTargetHeatingCoolingState.bind(this));
+  thermostatService
+    .getCharacteristic(Characteristic.TargetHeatingCoolingState)
+    .on("get", this.getTargetHeatingCoolingState.bind(this))
+    .on("set", this.setTargetHeatingCoolingState.bind(this));
 
-    thermostatService
-        .getCharacteristic(Characteristic.CurrentTemperature)
-        .on("get", this.getCurrentTemperature.bind(this))
-        .on("set", this.setCurrentTemperature.bind(this));
+  thermostatService
+    .getCharacteristic(Characteristic.CurrentTemperature)
+    .on("get", this.getCurrentTemperature.bind(this))
+    .on("set", this.setCurrentTemperature.bind(this));
 
-    thermostatService
-        .getCharacteristic(Characteristic.TargetTemperature)
-        .on("get", this.getTargetTemperature.bind(this))
-        .on("set", this.setTargetTemperature.bind(this));
+  thermostatService
+    .getCharacteristic(Characteristic.TargetTemperature)
+    .on("get", this.getTargetTemperature.bind(this))
+    .on("set", this.setTargetTemperature.bind(this));
 
-    this.informationService = infoService;
-    this.thermostatService = thermostatService;
+  this.informationService = infoService;
+  this.thermostatService = thermostatService;
 
-    return [this.informationService, this.thermostatService];
+  setInterval(() => {
+    bacnetHelper
+      .readAnalogInput(
+        "192.168.1.147",
+        0,
+        bacnet.enum.PropertyIds.PROP_PRESENT_VALUE
+      )
+      .then((value) => {
+        this.thermostatService
+          .getCharacteristic(Characteristic.CurrentTemperature)
+          .updateValue(value["values"]["value"]);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, 1000 * 15);
+
+  return [this.informationService, this.thermostatService];
 };
 
 myThermostat.prototype.getCurrentHeatingCoolingState = function (callback) {
-    return callback(null, this.currentHeatingCoolingState);
+  return callback(null, this.currentHeatingCoolingState);
 };
 
-myThermostat.prototype.setCurrentHeatingCoolingState = function (value, callback) {
-    this.currentHeatingCoolingState = value;
-    this.thermostatService.getCharacteristic(Characteristic.CurrentHeatingCoolingState).updateValue(value);
-    return callback(null);
+myThermostat.prototype.setCurrentHeatingCoolingState = function (
+  value,
+  callback
+) {
+  this.currentHeatingCoolingState = value;
+  this.thermostatService
+    .getCharacteristic(Characteristic.CurrentHeatingCoolingState)
+    .updateValue(value);
+  return callback(null);
 };
 
 myThermostat.prototype.getTargetHeatingCoolingState = function (callback) {
-    console.log(`GET Target heating/cooling state: ${this.targetHeatingCoolingState}`);
-    return callback(null, this.TargetHeatingCoolingState);
+  console.log(
+    `GET Target heating/cooling state: ${this.targetHeatingCoolingState}`
+  );
+  return callback(null, this.TargetHeatingCoolingState);
 };
 
-myThermostat.prototype.setTargetHeatingCoolingState = function (value, callback) {
-    this.targetHeatingCoolingState = value;
-    this.thermostatService.getCharacteristic(Characteristic.TargetHeatingCoolingState).updateValue(value);
+myThermostat.prototype.setTargetHeatingCoolingState = function (
+  value,
+  callback
+) {
+  this.targetHeatingCoolingState = value;
+  this.thermostatService
+    .getCharacteristic(Characteristic.TargetHeatingCoolingState)
+    .updateValue(value);
 
-    switch (this.targetHeatingCoolingState) {
-        case 0:
-            this.setCurrentHeatingCoolingState(0, () => { });
-            break;
+  switch (this.targetHeatingCoolingState) {
+    case 0:
+      this.setCurrentHeatingCoolingState(0, () => {});
+      break;
 
-        case 1:
-            this.setCurrentHeatingCoolingState(1, () => { });
-            break;
+    case 1:
+      this.setCurrentHeatingCoolingState(1, () => {});
+      break;
 
-        case 2:
-            this.setCurrentHeatingCoolingState(2, () => { });
-            break;
+    case 2:
+      this.setCurrentHeatingCoolingState(2, () => {});
+      break;
 
-        case 3:
-            if (this.targetTempearture > this.currentTemperature) {
-                this.setCurrentHeatingCoolingState(1, () => { });
-            } else if (this.targetTempearture < this.currentTemperature) {
-                this.setCurrentHeatingCoolingState(2, () => { });
-            } else {
-                this.setCurrentHeatingCoolingState(0, () => { });
-            }
-            break;
+    case 3:
+      if (this.targetTempearture > this.currentTemperature) {
+        this.setCurrentHeatingCoolingState(1, () => {});
+      } else if (this.targetTempearture < this.currentTemperature) {
+        this.setCurrentHeatingCoolingState(2, () => {});
+      } else {
+        this.setCurrentHeatingCoolingState(0, () => {});
+      }
+      break;
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 
-    console.log(`SET Target heating/cooling state: ${this.targetHeatingCoolingState}`);
-    console.log(`Current heating/cooling state: ${this.currentHeatingCoolingState}`);
-    return callback(null);
+  console.log(
+    `SET Target heating/cooling state: ${this.targetHeatingCoolingState}`
+  );
+  console.log(
+    `Current heating/cooling state: ${this.currentHeatingCoolingState}`
+  );
+  return callback(null);
 };
 
 myThermostat.prototype.getCurrentTemperature = function (callback) {
-    console.log(`GET Current temperature: ${this.currentTemperature}`);
-    return callback(null, this.currentTemperature);
+  console.log(`GET Current temperature: ${this.currentTemperature}`);
+  return callback(null, this.currentTemperature);
 };
 
 myThermostat.prototype.setCurrentTemperature = function (value, callback) {
-    this.currentTemperature = value;
-    console.log(`GET Current temperature: ${this.currentTemperature}`);
-    return callback(null);
+  this.currentTemperature = value;
+  console.log(`GET Current temperature: ${this.currentTemperature}`);
+  return callback(null);
 };
 
 myThermostat.prototype.getTargetTemperature = function (callback) {
-    console.log(`GET Target temperature: ${this.targetTempearture}`);
-    return callback(null, this.targetTempearture);
+  console.log(`GET Target temperature: ${this.targetTempearture}`);
+  return callback(null, this.targetTempearture);
 };
 
 myThermostat.prototype.setTargetTemperature = function (value, callback) {
-    this.targetTempearture = value;
-    console.log(`SET Target temperature: ${this.targetTempearture}`);
-    console.log(`Target heating/cooling state: ${this.targetHeatingCoolingState}`);
-    console.log(`Current heating/cooling state: ${this.currentHeatingCoolingState}`);
+  this.targetTempearture = value;
+  console.log(`SET Target temperature: ${this.targetTempearture}`);
+  console.log(
+    `Target heating/cooling state: ${this.targetHeatingCoolingState}`
+  );
+  console.log(
+    `Current heating/cooling state: ${this.currentHeatingCoolingState}`
+  );
 
-    if (this.targetTempearture > this.currentTemperature && this.targetHeatingCoolingState == 3) {
-        this.setCurrentHeatingCoolingState(1, () => { });
-    } else if (this.targetTempearture < this.currentTemperature && this.targetHeatingCoolingState == 3) {
-        this.setCurrentHeatingCoolingState(2, () => { });
-    } else if (this.targetHeatingCoolingState == 3) {
-        this.setCurrentHeatingCoolingState(0, () => { });
-    }
+  if (
+    this.targetTempearture > this.currentTemperature &&
+    this.targetHeatingCoolingState == 3
+  ) {
+    this.setCurrentHeatingCoolingState(1, () => {});
+  } else if (
+    this.targetTempearture < this.currentTemperature &&
+    this.targetHeatingCoolingState == 3
+  ) {
+    this.setCurrentHeatingCoolingState(2, () => {});
+  } else if (this.targetHeatingCoolingState == 3) {
+    this.setCurrentHeatingCoolingState(0, () => {});
+  }
 
-    return callback(null);
+  return callback(null);
 };
 
 module.exports = function (homebridge) {
-    Service = homebridge.hap.Service;
-    Characteristic = homebridge.hap.Characteristic;
-    UUID = homebridge.hap.uuid;
-    return myThermostat;
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  UUID = homebridge.hap.uuid;
+  return myThermostat;
 };
--- bacnet.js
+++ bacnet.js
@@ -3,127 +3,146 @@
 /// <reference path="./node_modules/bacstack/lib/enum.js" />
 
 const bacnet = require("bacstack");
-const EventEmitter = require("events");
 
 const client = new bacnet();
 
-client.on("iAm", (device) => {
-    console.log('address: ', device.address, ' - deviceId: ', device.deviceId, ' - maxAdpu: ', device.maxAdpu, ' - segmentation: ', device.segmentation, ' - vendorId: ', device.vendorId);
-});
-
-client.whoIs();
-
-let obj = {
-    type: bacnet.enum.ObjectTypes.OBJECT_ANALOG_INPUT,
-    instance: 0
-};
-
-client.readProperty("192.168.1.147", obj, bacnet.enum.PropertyIds.PROP_PRESENT_VALUE, (err, value) => {
-    if (err) {
-        console.error(err);
-    }
-
-    console.log(value);
-});
-
 function readBinaryInput(ipAddress, instance, propertyId) {
-    return new Promise((resolve, reject) => {
-        const propertyObject = {
-            type: bacnet.enum.ObjectTypes.OBJECT_BINARY_INPUT,
-            instance: instance
-        };
+  return new Promise((resolve, reject) => {
+    const propertyObject = {
+      type: bacnet.enum.ObjectTypes.OBJECT_BINARY_INPUT,
+      instance: instance,
+    };
 
-        client.readProperty(ipAddress, propertyObject, propertyId, (error, value) => {
-            if (error) {
-                return reject(error);
-            }
+    client.readProperty(
+      ipAddress,
+      propertyObject,
+      propertyId,
+      (error, value) => {
+        if (error) {
+          return reject(error);
+        }
 
-            return resolve(value);
-        });
-    });
+        return resolve(value);
+      }
+    );
+  });
 }
 
 function readBinaryValue(ipAddress, instance, propertyId) {
-    return new Promise((resolve, reject) => {
-        const propertyObject = {
-            type: bacnet.enum.ObjectTypes.OBJECT_BINARY_VALUE,
-            instance: instance
-        };
+  return new Promise((resolve, reject) => {
+    const propertyObject = {
+      type: bacnet.enum.ObjectTypes.OBJECT_BINARY_VALUE,
+      instance: instance,
+    };
 
-        client.readProperty(ipAddress, propertyObject, propertyId, (error, value) => {
-            if (error) {
-                return reject(error);
-            }
+    client.readProperty(
+      ipAddress,
+      propertyObject,
+      propertyId,
+      (error, value) => {
+        if (error) {
+          return reject(error);
+        }
 
-            return resolve(value);
-        });
-    });
+        return resolve(value);
+      }
+    );
+  });
 }
 
 function readBinaryOutput(ipAddress, instance, propertyId) {
-    return new Promise((resolve, reject) => {
-        const propertyObject = {
-            type: bacnet.enum.ObjectTypes.OBJECT_BINARY_OUTPUT,
-            instance: instance
-        };
+  return new Promise((resolve, reject) => {
+    const propertyObject = {
+      type: bacnet.enum.ObjectTypes.OBJECT_BINARY_OUTPUT,
+      instance: instance,
+    };
 
-        client.readProperty(ipAddress, propertyObject, propertyId, (error, value) => {
-            if (error) {
-                return reject(error);
-            }
+    client.readProperty(
+      ipAddress,
+      propertyObject,
+      propertyId,
+      (error, value) => {
+        if (error) {
+          return reject(error);
+        }
 
-            return resolve(value);
-        });
-    });
+        return resolve(value);
+      }
+    );
+  });
 }
 
 function readAnalogInput(ipAddress, instance, propertyId) {
-    return new Promise((resolve, reject) => {
-        const propertyObject = {
-            type: bacnet.enum.ObjectTypes.OBJECT_ANALOG_INPUT,
-            instance: instance
-        };
+  return new Promise((resolve, reject) => {
+    const propertyObject = {
+      type: bacnet.enum.ObjectTypes.OBJECT_ANALOG_INPUT,
+      instance: instance,
+    };
 
-        client.readProperty(ipAddress, propertyObject, propertyId, (error, value) => {
-            if (error) {
-                return reject(error);
-            }
+    client.readProperty(
+      ipAddress,
+      propertyObject,
+      propertyId,
+      (error, value) => {
+        if (error) {
+          return reject(error);
+        }
 
-            return resolve(value);
-        });
-    });
+        return resolve(value);
+      }
+    );
+  });
 }
 
 function readAnalogValue(ipAddress, instance, propertyId) {
-    return new Promise((resolve, reject) => {
-        const propertyObject = {
-            type: bacnet.enum.ObjectTypes.OBJECT_ANALOG_VALUE,
-            instance: instance
-        };
+  return new Promise((resolve, reject) => {
+    const propertyObject = {
+      type: bacnet.enum.ObjectTypes.OBJECT_ANALOG_VALUE,
+      instance: instance,
+    };
 
-        client.readProperty(ipAddress, propertyObject, propertyId, (error, value) => {
-            if (error) {
-                return reject(error);
-            }
+    client.readProperty(
+      ipAddress,
+      propertyObject,
+      propertyId,
+      (error, value) => {
+        if (error) {
+          return reject(error);
+        }
 
-            return resolve(value);
-        });
-    });
+        return resolve(value);
+      }
+    );
+  });
 }
 
 function readAnalogOutput(ipAddress, instance, propertyId) {
-    return new Promise((resolve, reject) => {
-        const propertyObject = {
-            type: bacnet.enum.ObjectTypes.OBJECT_ANALOG_OUTPUT,
-            instance: instance
-        };
+  return new Promise((resolve, reject) => {
+    const propertyObject = {
+      type: bacnet.enum.ObjectTypes.OBJECT_ANALOG_OUTPUT,
+      instance: instance,
+    };
 
-        client.readProperty(ipAddress, propertyObject, propertyId, (error, value) => {
-            if (error) {
-                return reject(error);
-            }
+    client.readProperty(
+      ipAddress,
+      propertyObject,
+      propertyId,
+      (error, value) => {
+        if (error) {
+          return reject(error);
+        }
 
-            return resolve(value);
-        });
-    });
+        return resolve(value);
+      }
+    );
+  });
 }
+
+module.exports = {
+  readAnalogInput: readAnalogInput,
+  readAnalogOutput: readAnalogOutput,
+  readAnalogValue: readAnalogValue,
+  readBinaryInput: readBinaryInput,
+  readBinaryOutput: readBinaryOutput,
+  readBinaryValue: readBinaryValue,
+};
