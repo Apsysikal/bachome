005073ba111c18f55c5f2ded108f145f4b93751a
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2020-06-13 19:43:06 +0200

    Added basic cooler.

---
+++ src/accessories/cooler.ts
@@ -0,0 +1,96 @@
+import { Service, PlatformAccessory } from 'homebridge';
+
+import { ExampleHomebridgePlatform } from '../platform';
+
+/**
+ * Platform Accessory
+ * An instance of this class is created for each accessory your platform registers
+ * Each accessory may expose multiple services of different service types.
+ */
+export class BachomeHeaterCoolerAccessory {
+  private service: Service;
+
+  private exampleStates = {
+    active: 0,
+    currentHeaterCoolerState: 0,
+    targetHeaterCoolerState: 0,
+    currentTemperature: 20.5,
+  }
+
+  constructor(
+    private readonly platform: ExampleHomebridgePlatform,
+    private readonly accessory: PlatformAccessory,
+  ) {
+
+    // set accessory information
+    this.accessory.getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, accessory.context.device.manufacturer)
+      .setCharacteristic(this.platform.Characteristic.Model, accessory.context.device.model)
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, accessory.context.device.serial);
+
+    this.service = this.accessory.getService(this.platform.Service.HeaterCooler)
+      || this.accessory.addService(this.platform.Service.HeaterCooler);
+
+    // To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
+    // when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
+    // this.accessory.getService('NAME') ?? this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE');
+
+    // Read the service name form the accessory context (config file passed via platform)
+    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
+
+    // register handlers for mandatory characteristics
+    this.service.getCharacteristic(this.platform.Characteristic.Active)
+      .on('set', this.setActive.bind(this))
+      .on('get', this.getActive.bind(this));
+
+    this.service.getCharacteristic(this.platform.Characteristic.CurrentHeaterCoolerState)
+      .on('get', this.getCurrentHeaterCoolerState.bind(this));
+
+    this.service.getCharacteristic(this.platform.Characteristic.TargetHeaterCoolerState)
+      .on('set', this.setTargetHeaterCoolerState.bind(this))
+      .on('get', this.getTargetHeaterCoolerState.bind(this));
+    
+    this.service.getCharacteristic(this.platform.Characteristic.CurrentTemperature)
+      .on('get', this.getCurrentTemperature.bind(this));
+  }
+
+  getActive(callback) {
+    this.platform.log.debug('GET Active');
+
+    callback(null, this.exampleStates.active);
+  }
+
+  setActive(value, callback) {
+    this.platform.log.debug('SET Active');
+
+    this.exampleStates.active = value;
+
+    callback(null);
+  }
+
+  getCurrentHeaterCoolerState(callback) {
+    this.platform.log.debug('GET CurrentHeaterCoolerState');
+
+    callback(null, this.exampleStates.currentHeaterCoolerState);
+  }
+
+  getTargetHeaterCoolerState(callback) {
+    this.platform.log.debug('GET TargetHeaterCoolerState');
+
+    callback(null, this.exampleStates.targetHeaterCoolerState);
+  }
+
+  setTargetHeaterCoolerState(value, callback) {
+    this.platform.log.debug('SET TargetHeaterCoolerState');
+
+    this.exampleStates.targetHeaterCoolerState = value;
+
+    callback(null);
+  }
+
+  getCurrentTemperature(callback) {
+    this.platform.log.debug('GET CurrentTemperature');
+
+    callback(null, this.exampleStates.currentTemperature);
+  }
+}
--- src/platform.ts
+++ src/platform.ts
@@ -11,6 +11,7 @@ import {
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 import { BachomeSwitchAccessory } from './accessories/switch';
 import { BachomeThermostatAccessory } from './accessories/thermostat';
+import { BachomeHeaterCoolerAccessory } from './accessories/cooler';
 
 /**
  * HomebridgePlatform
@@ -63,16 +64,6 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
     // EXAMPLE ONLY
     // A real plugin you would discover accessories from the local network, cloud services
     // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
 
     const exampleConfig = {
       switch: [
@@ -89,6 +80,14 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
           manufacturer: 'Example Manufacturer',
           model: 'Example Model',
           serial: 'LMN',
+        },
+      ],
+      cooler: [
+        {
+          name: 'Example Cooler One',
+          manufacturer: 'Example Manufacturer',
+          model: 'Example Model',
+          serial: 'OPQ',
         },
       ],
     };
@@ -143,59 +142,29 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
       }
     }
 
-    // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+    for (const device of exampleConfig.cooler) {
+      const uuid = this.api.hap.uuid.generate(device.serial);
 
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
       const existingAccessory = this.accessories.find(
         (accessory) => accessory.UUID === uuid,
       );
 
+
       if (existingAccessory) {
-        // the accessory already exists
-        this.log.info(
-          'Restoring existing accessory from cache:',
-          existingAccessory.displayName,
-        );
+        this.log.info(`Restoring existing accessory from cache: ${existingAccessory.displayName}`);
 
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
-
-        // create the accessory handler for the restored accessory
-        // this is imported from `platformAccessory.ts`
-        new BachomeSwitchAccessory(this, existingAccessory);
+        new BachomeHeaterCoolerAccessory(this, existingAccessory);
       } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
+        this.log.info(`Adding new accessory: ${device.name}`);
 
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(
-          device.exampleDisplayName,
-          uuid,
-        );
+        const accessory = new this.api.platformAccessory(device.name, uuid);
 
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
         accessory.context.device = device;
 
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new BachomeSwitchAccessory(this, accessory);
+        new BachomeHeaterCoolerAccessory(this, accessory);
 
-        // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [
-          accessory,
-        ]);
+        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
       }
-
-      // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-      // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
     }
   }
 }
